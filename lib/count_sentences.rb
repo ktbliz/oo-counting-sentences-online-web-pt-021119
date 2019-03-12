26095479abc52c34de0398f50e60750e867a73f3
Katie Blizzard <kblizzard11@gmail.com>
2019-03-12 16:07:57 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? TRUE : FALSE 
   end
 
   def question?
-
+    self.end_with?("?") ? TRUE : FALSE 
   end
 
   def exclamation?
-
+    self.end_with?("!") ? TRUE : FALSE 
   end
 
   def count_sentences
-
-  end
+    arr = self.split
+    arr.count { |x| x.end_with?(".") || x.end_with?("?") || x.end_with?("!")}
+  end  
 end
